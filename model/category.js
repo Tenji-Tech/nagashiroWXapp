889f7349e94f1ade70160ef1110ed2e8eacd216b
樊一博 <fanyibo66@ten-ji.co.jp>
2024-06-06 15:11:14 +0900

    config CAT and each images

--- model/category.js
+++ model/category.js
@@ -1,234 +1,416 @@
 export function getCategoryList() {
   return [
     {
-      groupId: '24948',
+      groupId: 'Z',
       name: '应季食材',
-      thumbnail:
-        'https://cdn-we-retail.ym.tencent.com/miniapp/category/category-default.png',
+      thumbnail:'',
       children: [
         {
-          groupId: '249481',
+          groupId: 'Z0',
           name: ' ',
-          thumbnail:
-            'https://cdn-we-retail.ym.tencent.com/miniapp/category/category-default.png',
+          thumbnail: '',
           children: [
             {
-              groupId: '249480',
-              name: '卫衣',
-              thumbnail:
-                'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-1.png',
+              groupId: 'Z1',
+              name: '鲜鱼',
+              thumbnail:'/images/cat/Z/Z1.png',
             },
             {
-              groupId: '249480',
-              name: '毛呢外套',
+              groupId: 'Z2',
+              name: '活鱼',
               thumbnail:
-                'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-2.png',
+                '/images/cat/Z/Z2.png',
             },
             {
-              groupId: '249480',
-              name: '雪纺衫',
+              groupId: 'Z3',
+              name: '贝类',
               thumbnail:
-                'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-3.png',
+                '/images/cat/Z/Z3.png',
             },
             {
-              groupId: '249480',
-              name: '羽绒服',
+              groupId: 'Z4',
+              name: '蔬菜',
               thumbnail:
-                'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-4.png',
+                '/images/cat/Z/Z4.png',
             },
             {
-              groupId: '249480',
-              name: '毛衣',
+              groupId: 'Z5',
+              name: '水果',
               thumbnail:
-                'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-5.png',
-            },
-            {
-              groupId: '249480',
-              name: '棉衣',
-              thumbnail:
-                'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-6.png',
-            },
-            {
-              groupId: '249480',
-              name: '西装',
-              thumbnail:
-                'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-7.png',
-            },
-            {
-              groupId: '249480',
-              name: '马甲',
-              thumbnail:
-                'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-8.png',
-            },
-            {
-              groupId: '249480',
-              name: '连衣裙',
-              thumbnail:
-                'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-9.png',
-            },
-            {
-              groupId: '249480',
-              name: '半身裙',
-              thumbnail:
-                'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-10.png',
-            },
-            {
-              groupId: '249480',
-              name: '裤子',
-              thumbnail:
-                'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-11.png',
+                '/images/cat/Z/Z5.png',
             },
           ],
         },
       ],
     },
     {
-      groupId: '24948',
+      groupId: 'A',
       name: '鲜鱼类',
-      thumbnail:
-        'https://cdn-we-retail.ym.tencent.com/miniapp/category/category-default.png',
+      thumbnail: '',
       children: [
         {
-          groupId: '249481',
+          groupId: 'A0',
           name: ' ',
-          thumbnail:
-            'https://cdn-we-retail.ym.tencent.com/miniapp/category/category-default.png',
+          thumbnail: '',
           children: [
             {
-              groupId: '249480',
-              name: '卫衣',
+              groupId: 'A1',
+              name: '青花鱼类',
               thumbnail:
-                'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-1.png',
+              '/images/cat/A/A1.png',
             },
             {
-              groupId: '249480',
-              name: '裤子',
+              groupId: 'A2',
+              name: '鳕鱼科',
               thumbnail:
-                'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-11.png',
+              '/images/cat/A/A2.png',
             },
             {
-              groupId: '249480',
-              name: '西装',
+              groupId: 'A3',
+              name: '金枪鱼',
               thumbnail:
-                'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-7.png',
+              '/images/cat/A/A3.png',
             },
             {
-              groupId: '249480',
-              name: '羽绒服',
+              groupId: 'A4',
+              name: '鲈鱼科',
               thumbnail:
-                'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-4.png',
+              '/images/cat/A/A4.png',
             },
             {
-              groupId: '249480',
-              name: '马甲',
+              groupId: 'A5',
+              name: '鲑鱼科',
               thumbnail:
-                'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-8.png',
+              '/images/cat/A/A5.png',
+            },
+            {
+              groupId: 'A6',
+              name: '比目鱼科',
+              thumbnail:
+              '/images/cat/A/A6.png',
+            },
+            {
+              groupId: 'A7',
+              name: '海胆',
+              thumbnail:
+              '/images/cat/A/A7.png',
+            },
+            {
+              groupId: 'A8',
+              name: '特殊部位',
+              thumbnail:
+              '/images/cat/A/A8.png',
             },
           ],
         },
       ],
     },
     {
-      groupId: '24948',
+      groupId: 'B',
       name: '其他海鲜',
       thumbnail:
-        'https://cdn-we-retail.ym.tencent.com/miniapp/category/category-default.png',
+        '',
       children: [
         {
-          groupId: '249481',
+          groupId: 'B0',
           name: ' ',
           thumbnail:
-            'https://cdn-we-retail.ym.tencent.com/miniapp/category/category-default.png',
+            '',
           children: [
             {
-              groupId: '249480',
-              name: '马甲',
+              groupId: 'B1',
+              name: '鱿鱼类',
               thumbnail:
-                'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-8.png',
+              '/images/cat/B/B1.png',
             },
             {
-              groupId: '249480',
-              name: '裤子',
+              groupId: 'B2',
+              name: '虾类',
               thumbnail:
-                'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-11.png',
+              '/images/cat/B/B2.png',
             },
             {
-              groupId: '249480',
-              name: '连衣裙',
+              groupId: 'B3',
+              name: '贝类',
               thumbnail:
-                'https://cdn-we-retail.ym.tencent.com/tsr/classify/img-9.png',
+              '/images/cat/B/B3.png',
             },
             {
-              groupId: '249480',
-              name: '其他',
+              groupId: 'B4',
+              name: '蟹类',
               thumbnail:
-                'https://cdn-we-retail.ym.tencent.com/tsr/goods/muy-3b.png',
+              '/images/cat/B/B4.png',
             },
           ],
         },
       ],
     },
     {
-      groupId: '24948',
+      groupId: 'C',
       name: '冷冻品',
       thumbnail:
-        'https://cdn-we-retail.ym.tencent.com/miniapp/category/category-default.png',
+        '',
       children: [
         {
-          groupId: '249481',
+          groupId: 'C0',
           name: ' ',
           thumbnail:
-            'https://cdn-we-retail.ym.tencent.com/miniapp/category/category-default.png',
+            '',
           children: [
             {
-              groupId: '249480',
-              name: '唇釉',
+              groupId: 'C1',
+              name: '鱼类',
               thumbnail:
-                'https://cdn-we-retail.ym.tencent.com/tsr/goods/mz-20a1.png',
+              '/images/cat/C/C1.png',
             },
             {
-              groupId: '249480',
-              name: '美妆蛋',
+              groupId: 'C2',
+              name: '鱿鱼类',
               thumbnail:
-                'https://cdn-we-retail.ym.tencent.com/tsr/goods/mz-11a1.png',
+              '/images/cat/C/C2.png',
             },
             {
-              groupId: '249480',
-              name: '眼影',
+              groupId: 'C3',
+              name: '虾类',
               thumbnail:
-                'https://cdn-we-retail.ym.tencent.com/tsr/goods/mz-12b.png',
+              '/images/cat/C/C3.png',
+            },
+            {
+              groupId: 'C4',
+              name: '加工食品',
+              thumbnail:
+              '/images/cat/C/C4.png',
             },
           ],
         },
       ],
     },
     {
-      groupId: '24949',
+      groupId: 'D',
       name: '蔬菜类',
-      thumbnail:
-        'https://cdn-we-retail.ym.tencent.com/miniapp/category/category-default.png',
-      children: []
+      thumbnail:  '',
+      children: [{
+        groupId: 'D0',
+        name: ' ',
+        thumbnail:
+          '',
+        children: [
+          {
+            groupId: 'D1',
+            name: '茄果类',
+            thumbnail:
+            '/images/cat/D/D1.png',
+          },
+          {
+            groupId: 'D2',
+            name: '芋头类',
+            thumbnail:
+            '/images/cat/D/D2.png',
+          },
+          {
+            groupId: 'D3',
+            name: '根菜类',
+            thumbnail:
+            '/images/cat/D/D3.png',
+          },
+          {
+            groupId: 'D4',
+            name: '根茎类',
+            thumbnail:
+            '/images/cat/D/D4.png',
+          },
+          {
+            groupId: 'D5',
+            name: '菌菇类',
+            thumbnail:
+            '/images/cat/D/D5.png',
+          },
+          {
+            groupId: 'D6',
+            name: '豆类',
+            thumbnail:
+            '/images/cat/D/D6.png',
+          },
+          {
+            groupId: 'D7',
+            name: '叶菜类',
+            thumbnail:
+            '/images/cat/D/D7.png',
+          },
+          {
+            groupId: 'D8',
+            name: '山菜类',
+            thumbnail:
+            '/images/cat/D/D8.png',
+          },
+          {
+            groupId: 'D9',
+            name: '食用花',
+            thumbnail:
+            '/images/cat/D/D9.png',
+          },
+        ],
+      },]
     },
     {
-      groupId: '24950',
+      groupId: 'E',
       name: '水果类',
       thumbnail:
-        'https://cdn-we-retail.ym.tencent.com/miniapp/category/category-default.png',
-      children: []
+        '',
+      children: [{
+        groupId: 'E0',
+        name: ' ',
+        thumbnail:
+          '',
+        children: [
+          {
+            groupId: 'E01',
+            name: '草莓',
+            thumbnail:
+            '/images/cat/E/E01.png',
+          },
+          {
+            groupId: 'E02',
+            name: '哈蜜瓜',
+            thumbnail:
+            '/images/cat/E/E02.png',
+          },
+          {
+            groupId: 'E03',
+            name: '葡萄',
+            thumbnail:
+            '/images/cat/E/E03.png',
+          },
+          {
+            groupId: 'E04',
+            name: '梨',
+            thumbnail:
+            '/images/cat/E/E04.png',
+          },
+          {
+            groupId: 'E05',
+            name: '桃',
+            thumbnail:
+            '/images/cat/E/E05.png',
+          },
+          {
+            groupId: 'E06',
+            name: '苹果',
+            thumbnail:
+            '/images/cat/E/E06.png',
+          },
+          {
+            groupId: 'E07',
+            name: '柿子',
+            thumbnail:
+            '/images/cat/E/E07.png',
+          },
+          {
+            groupId: 'E08',
+            name: '梅',
+            thumbnail:
+            '/images/cat/E/E08.png',
+          },
+          {
+            groupId: 'E09',
+            name: '栗子',
+            thumbnail:
+            '/images/cat/E/E09.png',
+          },
+          {
+            groupId: 'E10',
+            name: '柑橘',
+            thumbnail:
+            '/images/cat/E/E10.png',
+          },
+          {
+            groupId: 'E11',
+            name: '其他',
+            thumbnail:
+            '/images/cat/E/E11.png',
+          },
+          {
+            groupId: 'E12',
+            name: '装饰用',
+            thumbnail:
+            '/images/cat/E/E12.png',
+          },
+        ],
+      },]
     },
     {
-      groupId: '24951',
+      groupId: 'F',
       name: '加工品',
-      thumbnail:
-        'https://cdn-we-retail.ym.tencent.com/miniapp/category/category-default.png',
-      children: []
+      thumbnail: '',
+      children: [{
+        groupId: 'F0',
+        name: ' ',
+        thumbnail:
+          '',
+        children: [
+          {
+            groupId: 'F1',
+            name: '蔬菜腌渍',
+            thumbnail:
+            '/images/cat/F/F1.png',
+          },
+          {
+            groupId: 'F2',
+            name: '鱼类腌渍',
+            thumbnail:
+            '/images/cat/F/F2.png',
+          },
+          {
+            groupId: 'F3',
+            name: '其他',
+            thumbnail:
+            '/images/cat/F/F3.png',
+          },
+          {
+            groupId: 'F4',
+            name: '干货',
+            thumbnail:
+            '/images/cat/F/F4.png',
+          },
+          {
+            groupId: 'F5',
+            name: '加工水果',
+            thumbnail:
+            '/images/cat/F/F5.png',
+          },
+        ],
+      },]
     },
     {
-      groupId: '24952',
+      groupId: 'G',
       name: '其他',
-      thumbnail:
-        'https://cdn-we-retail.ym.tencent.com/miniapp/category/category-default.png',
-      children: []
+      thumbnail: '',
+      children: [{
+        groupId: 'G0',
+        name: ' ',
+        thumbnail:
+          '',
+        children: [
+          {
+            groupId: 'G1',
+            name: '调料品',
+            thumbnail:
+            '/images/cat/G/G1.png',
+          },
+          {
+            groupId: 'G2',
+            name: '厨房器具',
+            thumbnail:
+            '/images/cat/G/G2.png',
+          },
+          {
+            groupId: 'G3',
+            name: '消耗品',
+            thumbnail:
+            '/images/cat/G/G3.png',
+          },
+        ],
+      },]
     },
     
   ];
